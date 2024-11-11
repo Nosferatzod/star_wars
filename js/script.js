4960523046f25db2f32b516b9b145ac069f9d269
Loner <163582383+Nosferatzod@users.noreply.github.com>
2024-11-11 16:45:24 -0300

    star_wars

--- js/script.js
+++ js/script.js
@@ -7,4 +7,16 @@ function controlarMusica() {
         audio.pause();
     }
 }
+const toggleButton = document.getElementById('toggleButton');
+const navbarNav = document.getElementById('navbarNav');
+const closeButton = document.getElementById('closeButton');
+
+toggleButton.addEventListener('click', () => {
+    navbarNav.classList.toggle('active');
+});
+
+closeButton.addEventListener('click', () => {
+    navbarNav.classList.remove('active');
+});
+
 
